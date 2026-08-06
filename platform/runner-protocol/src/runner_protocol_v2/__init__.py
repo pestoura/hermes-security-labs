e914aef602928ac619ec436e8ae2708818322f6a
Pedro <pedro.estoura@gmail.com>
2026-08-06 07:52:42 +0100

    feat(epic-05): publish Runner Protocol SDK API

---
+++ platform/runner-protocol/src/runner_protocol_v2/__init__.py
@@ -0,0 +1,28 @@
+"""Public repository-local SDK for Runner Protocol v2."""
+
+from .contracts import (
+    ProtocolValidationError,
+    classify_idempotency,
+    contract_root,
+    load_schema,
+    request_fingerprint,
+    validate_compatibility_matrix,
+    validate_progress_sequence,
+    validate_schema,
+    validate_semantics,
+)
+
+__version__ = "2.0.0"
+
+__all__ = [
+    "ProtocolValidationError",
+    "__version__",
+    "classify_idempotency",
+    "contract_root",
+    "load_schema",
+    "request_fingerprint",
+    "validate_compatibility_matrix",
+    "validate_progress_sequence",
+    "validate_schema",
+    "validate_semantics",
+]
